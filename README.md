78beff59392912d806d1e55ae9941e36c382947f
mk2408 <70700654+Mitali8620@users.noreply.github.com>
2023-02-23 01:13:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# flutter_mvvm
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+Model–View–ViewModel (MVVM) 
+
+this project for flutter MVVM architecture
+this project provide small  for MVVM architectures 
+
+## Flutter version
+# version : 3.3.10
---
+++ lib/model/media_repository.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter_mvvm/model/services/base_service.dart';
+
+import 'media.dart';
+import 'services/media_service.dart';
+
+class MediaRepository {
+  BaseService _mediaService = MediaService();
+
+  Future<List<Media>> fetchMediaList(String value) async {
+    dynamic response = await _mediaService.getResponse(value);
+    final jsonData = response['results'] as List;
+    List<Media> mediaList =
+        jsonData.map((tagJson) => Media.fromJson(tagJson)).toList();
+    return mediaList;
+  }
+}
